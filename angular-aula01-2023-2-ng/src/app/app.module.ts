ad5932b210fb4d88579e413f3e39daa1cdc05f03
jacksonwsup <jacksonwsup@gmail.com>
2023-07-02 16:14:40 -0400

    ajuste

--- angular-aula01-2023-2-ng/src/app/app.module.ts
+++ angular-aula01-2023-2-ng/src/app/app.module.ts
@@ -1,36 +1,10 @@
-import { LOCALE_ID, NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
+import { Component } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ConsultaSaldoComponent } from './components/consulta-saldo/consulta-saldo.component';
-import { ClassNamePorValorPipe } from './pipes/class-name-por-valor.pipe';
-import { CardClienteComponent } from './components/card-cliente/card-cliente.component';
-
-registerLocaleData(localePt, 'pt-BR');
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ConsultaSaldoComponent,
-    ClassNamePorValorPipe,
-    CardClienteComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-  ],
-  providers: [
-    {
-      provide: LOCALE_ID,
-      useValue: 'pt-BR',
-    },
-  ],
-  bootstrap: [
-    AppComponent,
-  ],
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class AppModule {
+export class AppComponent {
+  title = 'angular-cartas';
 }
